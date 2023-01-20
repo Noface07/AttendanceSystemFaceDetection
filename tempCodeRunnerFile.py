5f220e62f1b1d6b857308bba845a4ee3948c41bf
Yuvraj Soni <82412523+Noface07@users.noreply.github.com>
2023-01-20 11:45:29 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+con = c.connect(host="localhost", user="root", passwd="1234", database="attendance_system")
+# cur=con.cursor()
+# def attendance(id, name, date):
+#     query="Insert into attendance values({}, '{}', {}, {})".format(id,name,date,1)
+#     cur.execute(query)
+#     con.commit()
